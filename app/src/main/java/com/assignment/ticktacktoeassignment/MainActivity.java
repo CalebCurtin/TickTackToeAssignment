653e9c550c8e54e3f287af9b3e88f2e6801e99c4
RafeUmair <raferaed98@gmail.com>
2023-08-31 11:56:04 +0800

    Implemented loading of main menu fragment.

--- app/src/main/java/com/assignment/ticktacktoeassignment/MainActivity.java
+++ app/src/main/java/com/assignment/ticktacktoeassignment/MainActivity.java
@@ -1,14 +1,37 @@
 package com.assignment.ticktacktoeassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import android.view.View;
+import android.os.Bundle;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private MenuFragment menuFragment = new MenuFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadInitialMenu();
+    }
+
+    private void loadInitialMenu() {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.mainMenu_Container);
+
+        View mainMenuContainer = findViewById(R.id.mainMenu_Container);
+        if (frag == null) {
+            fm.beginTransaction().add(R.id.mainMenu_Container, menuFragment).commit();
+        } else {
+            fm.beginTransaction().replace(R.id.mainMenu_Container, menuFragment).commit();
+        }
     }
 }
